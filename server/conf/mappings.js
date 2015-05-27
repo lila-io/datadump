601a518d559a63dfb996b5d8a7031906796ab7ac
Ivar Prudnikov <ivar.prudnikov@gmail.com>
2015-05-27 08:39:48 +0100

    added comments to mappings

--- server/conf/mappings.js
+++ server/conf/mappings.js
@@ -6,6 +6,9 @@ var RouteErrors = require('../services/RouteErrorsService');
 
 exports.init = function (app) {
 
+  /**
+   * CORS for api routes
+   */
   app.use(cfg.routes.api.root + '/*', function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'X-Requested-With, Authorization, origin, content-type, accept');
@@ -14,23 +17,29 @@ exports.init = function (app) {
   });
 
   /**
-   * Load <User>resourceOwner along with
+   * Load {User}resourceOwner along with
    * authenticated user to later determine if
-   * one has access to anothers' data
+   * one has access to others' data
    */
   app.use(cfg.routes.api.versionRoot + '/user/:userId/*', RouteUserLoader.load('userId'));
 
-  // setup routes for this app
+  /**
+   * Define all routes, register routers
+   */
   app.use('/', require('../routes/index'));
   app.use(cfg.routes.auth.path, require('../routes/auth'));
   app.use(cfg.routes.api.versionRoot + '/user/:userId/bucket/:bucketId/data', require('../routes/bucketItem'));
   app.use(cfg.routes.api.versionRoot + '/user/:userId/bucket', require('../routes/bucket'));
   app.use(cfg.routes.api.versionRoot + '/user/:userId/admin', require('../routes/admin'));
 
-  // 404 errors
+  /**
+   * 404 errors, unhandled routes
+   */
   app.use(RouteErrors.sendNotFound());
 
-  // Server errors
+  /**
+   * Catch errors and respond with status 500
+   */
   app.use(RouteErrors.catchServerErrors());
 
 };
